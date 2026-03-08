71d6fdd55c49f57364a56bbf1cf55794832b975c
Pharez <pharezoyelade@gmail.com>
2026-03-08 17:48:57 +0100

    landing layout setup

---
+++ frontend/src/app/(landing)/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from "@/components/ui/Button";
+import Input from "@/components/ui/Input";
+import Navbar from "@/components/layout/Navbar";
+
+const page = () => {
+  return <div></div>;
+};
+
+export default page;
